9961f9bb8b4c7f0758d5875fc3a700761b7c53a0
gurjindertoor <toortalk@gmail.com>
2024-06-11 18:11:38 -0500

    added loading screen for uploads
    -added loading screen for uploads
    -loading screen appears while uploading and disappears when upload completed

--- src/app/components/UploadForm.js
+++ src/app/components/UploadForm.js
@@ -1,13 +1,17 @@
 "use client";
 import UploadIcon from "./UploadIcon";
 import axios from "axios";
+import { useState } from "react";
 
 const UploadForm = () => {
+  const [isUploading, setIsUploading] = useState(false);
+
   async function upload(ev) {
     ev.preventDefault();
     const files = ev.target.files;
     if (files.length > 0) {
       const file = files[0];
+      setIsUploading(true);
       const formData = new FormData();
       formData.append("file", file);
 
@@ -17,6 +21,7 @@ const UploadForm = () => {
             "Content-Type": "multipart/form-data",
           },
         });
+        setIsUploading(false);
         console.log(res.data);
       } catch (error) {
         console.error("Error uploading file:", error);
@@ -25,11 +30,21 @@ const UploadForm = () => {
   }
 
   return (
-    <label className="bg-green-600 py-2 px-6 rounded-full inline-flex gap-2 border-2 border-purple-700/50 cursor-pointer">
-      <UploadIcon />
-      <span>Choose file</span>
-      <input onChange={upload} type="file" className="hidden" />
-    </label>
+    <>
+      {isUploading && (
+        <div className="bg-black/90 text-white fixed inset-0 flex items-center">
+          <div className="w-full text-center">
+            <h2 className="text-4xl mb-4">Uploading...</h2>
+            <h3 className="text-xl">Please Wait</h3>
+          </div>
+        </div>
+      )}
+      <label className="bg-green-600 py-2 px-6 rounded-full inline-flex gap-2 border-2 border-purple-700/50 cursor-pointer">
+        <UploadIcon />
+        <span>Choose file</span>
+        <input onChange={upload} type="file" className="hidden" />
+      </label>
+    </>
   );
 };
 
